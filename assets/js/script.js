48f0490544d5fb71b86ef6074e8d76618f6b8c9a
Vanessa Trinh <xtourmaline@gmail.com>
2023-10-29 17:05:28 -0400

    worked on javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+const apiKey = "8c8324b39583c855b4fe51feb2f0e639";
+let btnSearch = $("#search")
+
+btnSearch.on("click", function () {
+    let cityInput = $("#citySearch").value.trim();
+  
+    //checks to see if the user enters anything
+    if (cityInput === "") {
+      alert("Please enter a city");
+    } else {
+      cityInput = $("#citySearch").val();
+    }
+})
+
+// getting the lon, lat, and name from the API response
+function getLocation() {
+    let cityCoordinates = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=1&appid=${apiKey}`
+    
+    fetch(cityCoordinates)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        let name = data[0].name;
+        let lon = data[0].lon;
+        let lat = data[0].lat;
+
+        return name, lon, lat
+    })
+}
+
+let name, lon, lat = getLocation()
+
+// getting the weather
+function getWeather() {
+    let weather = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+    fetch(weather)
+        .then(function (response) {
+            return response.json();
+    })
+        .then(function (data) {
+            console.log(data);
+    });
+}
