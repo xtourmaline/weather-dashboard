576a7cc7ee8bf8212a1c16b976da080533dedca8
Vanessa Trinh <xtourmaline@gmail.com>
2023-11-02 02:43:39 -0400

    got the data to display in each card

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,9 @@ btnSearch.on("click", function () {
 
                 // calls function getWeather
                 getWeather(lon, lat, apiKey);
+
+                // displays the city name
+                $("#city-name").text(name);
             })
             .catch(error => {
                 console.error("Error:", error);
@@ -58,6 +61,57 @@ function getWeather(lon, lat, apiKey) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
+            console.log(data);  // FOR TESTING PURPOSES!! DELETE LATER
+        });
+}
+
+
+function getWeather(lon, lat, apiKey) {
+    let weather = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+    fetch(weather)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);  // FOR TESTING PURPOSES!! DELETE LATER
+
+            console.log(data.list[0])
+            console.log(data.list[0].weather[0])
+            console.log(data.list[0].weather[0].description)
+
+            let date = `
+                <p>${dayjs(data.list[0].dt_txt).format("MM/DD/YYYY")}</p>
+            `
+
+            let forecastContent = `
+                <img src="http://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png">
+                <p class="card-text">Temp: ${data.list[0].main.temp} ºC</p>
+                <p class="card-text">Wind: ${data.list[0].wind.speed} mph</p>
+                <p class="card-text">Humidity: ${data.list[0].main.humidity} g/kg</p>
+            `;
+            
+            $("#day0").children().first().after(date);
+            $("#day0 > :nth-child(2)").nextAll().remove();
+            
+            $("#day0").append(forecastContent);
+            
+            let count = 7;
+
+            for (let i = 1; i < 6; i++) {
+                forecastContent = `
+                    <img src="http://openweathermap.org/img/w/${data.list[count].weather[0].icon}.png">
+                    <p class="card-text">Temp: ${data.list[count].main.temp} ºC</p>
+                    <p class="card-text">Wind: ${data.list[count].wind.speed} mph</p>
+                    <p class="card-text">Humidity: ${data.list[count].main.humidity} g/kg</p>
+                `;
+
+                count += 8;
+                console.log(forecastContent)
+
+                let $dayElement = $(`#day${i}`);
+                $dayElement.children(":not(:first-child)").remove();
+                $dayElement.append(forecastContent);
+            }
         });
 }
